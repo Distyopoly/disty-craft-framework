245c087a3a47fce18bd67708c9fc80d00ebdb1f1
leswell <simofilevente+git@gmail.com>
2025-11-20 19:53:13 +0200

    chore: initialize husky for managing git hooks

--- devenv.nix
+++ devenv.nix
@@ -2,11 +2,14 @@
 {
   # https://devenv.sh/packages/
   packages = [
-    pkgs.nodejs
   ];
 
   # https://devenv.sh/languages/
-  languages.javascript.enable = true;
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_22;
+    pnpm.enable = true;
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 
