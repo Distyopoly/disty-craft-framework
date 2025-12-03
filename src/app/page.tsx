98a88776e76c68288a42dd69c8e20341ab1b9109
leswell <simofilevente+git@gmail.com>
2025-12-03 17:05:29 +0200

    feat(index-page): update to chakra-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import NextLink from "next/link";
-import { Link as ChakraLink } from '@chakra-ui/react'
+import { Button, Link as ChakraLink, Heading, VStack } from '@chakra-ui/react'
 
 
 export default function Home() {
   return (
-    <>
-      <h1>Welcome</h1>
-      <ChakraLink asChild>
+    <VStack>
+      <Heading>Welcome</Heading>
+      <Button asChild>
         <NextLink href="session">Start a new session</NextLink>
-      </ChakraLink>
-    </>
+      </Button>
+    </VStack>
   )
 }
