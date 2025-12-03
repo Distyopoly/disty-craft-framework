8ba3da322a71c118bb97c7d4e63b7fcedb0eabc8
leswell <simofilevente+git@gmail.com>
2025-12-03 18:24:24 +0200

    feat(index-page): centered content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import NextLink from "next/link";
-import { Button, Link as ChakraLink, Heading, VStack } from '@chakra-ui/react'
+import { Button, Center, Heading, VStack } from '@chakra-ui/react'
 
 
 export default function Home() {
   return (
-    <VStack>
-      <Heading>Welcome</Heading>
-      <Button asChild>
-        <NextLink href="session">Start a new session</NextLink>
-      </Button>
-    </VStack>
+    <Center h="100vh">
+      <VStack>
+        <Heading>Welcome</Heading>
+        <Button asChild>
+          <NextLink href="session">Start a new session</NextLink>
+        </Button>
+      </VStack>
+    </Center>
   )
 }
